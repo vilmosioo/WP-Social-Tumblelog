4bbbcb36ea28ba4d00f63247c86e12c04d0cb7ee
Vilmos Ioo <cool.villi@gmail.com>
2014-01-04 17:37:56 +0000

    Added tumblelog shortcode

--- wp-social-tumblelog.php
+++ wp-social-tumblelog.php
@@ -46,7 +46,56 @@ class WPSocialTumblelog_Plugin {
 		register_deactivation_hook(__FILE__, array( &$this, 'deactivate' ) );
 		
 		WPSocialTumblelog_Settings::init();
+		add_shortcode('tumblelog', array( &$this, 'display_tumblog' ));
 	} 
+
+	private function format_feed($url, $no){
+		// Get a SimplePie feed object from the specified feed source.
+		$rss = fetch_feed($url); // specify the source feed
+		if (!is_wp_error( $rss ) ) { // Checks that the object is created correctly 
+			// Figure out how many total items there are, but limit it to 5. 
+			$maxitems = $rss->get_item_quantity($no); 
+			// Build an array of all the items, starting with element 0 (first element).
+			$rss_items = $rss->get_items(0, $maxitems); 
+		} else {
+			return false;
+		}
+
+		$feed = array();
+		if ($maxitems != 0) {
+			foreach ($rss_items as $item ){
+				$feed[ strtotime($item->get_date()) ] = $item;
+			}
+		}
+		return $feed;
+  }
+
+	// display activity chart for a repository
+	public function display_tumblog($atts, $content = null){
+		extract(shortcode_atts(array('wrap_class' => '','item_class' => '', 'count' => 10), $atts));
+		  	
+  	$feed = array();
+  	$option = get_option(WPSocialTumblelog_Resources::DATA);
+  	foreach ($option as $key => $value) {
+  		$feed += $this->format_feed($value['url'], $count);
+  	}
+
+  	// sort items descending by date
+  	krsort($feed);
+
+		$s = "<div class='tumblelog $wrap_class'>";
+
+		foreach($feed as $item){
+			$s .= "<article class='tumblelog-item $item_class'>";
+			$s .= "<h3><a href='".$item->get_link()."' target='_blank'>".$item->get_title()."</a></h3>";
+			$s .= $item->get_description();
+			$s .= "</article>";
+		}
+
+		$s .= "</div>";
+
+		return $s;
+	}
 
 	/**
 	 * Fired when the plugin is activated.
